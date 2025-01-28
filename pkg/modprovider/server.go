9f6dc62975471c9680992c0747361b5992a1174e
Anton Tayanovskyy <anton@pulumi.com>
2025-01-28 14:42:02 -0500

    Flesh out the server lifecycle methods

--- pkg/modprovider/server.go
+++ pkg/modprovider/server.go
@@ -15,11 +15,95 @@
 package modprovider
 
 import (
+	"context"
+
 	"github.com/pulumi/pulumi/pkg/v3/resource/provider"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
 
 	pulumirpc "github.com/pulumi/pulumi/sdk/v3/proto/go"
 )
 
 func StartServer(hc *provider.HostClient) (pulumirpc.ResourceProviderServer, error) {
-	return nil, nil
+	return &server{}, nil
+}
+
+type server struct {
+	pulumirpc.UnimplementedResourceProviderServer
+}
+
+func (s *server) Parameterize(
+	ctx context.Context,
+	req *pulumirpc.ParameterizeRequest,
+) (*pulumirpc.ParameterizeResponse, error) {
+	panic("TODO")
+}
+
+func (s *server) GetSchema(
+	ctx context.Context,
+	req *pulumirpc.GetSchemaRequest,
+) (*pulumirpc.GetSchemaResponse, error) {
+	panic("TODO")
+}
+
+func (*server) GetPluginInfo(
+	ctx context.Context,
+	req *emptypb.Empty,
+) (*pulumirpc.PluginInfo, error) {
+	return &pulumirpc.PluginInfo{
+		Version: "1.0.0",
+	}, nil
+}
+
+func (*server) Configure(
+	ctx context.Context,
+	req *pulumirpc.ConfigureRequest,
+) (*pulumirpc.ConfigureResponse, error) {
+	return &pulumirpc.ConfigureResponse{
+		AcceptSecrets:   true,
+		SupportsPreview: true,
+		AcceptOutputs:   true,
+		AcceptResources: true,
+	}, nil
+}
+
+func (rps *server) Construct(
+	ctx context.Context,
+	req *pulumirpc.ConstructRequest,
+) (*pulumirpc.ConstructResponse, error) {
+	panic("TODO")
+}
+
+func (rps *server) Check(
+	ctx context.Context,
+	req *pulumirpc.CheckRequest,
+) (*pulumirpc.CheckResponse, error) {
+	panic("TODO")
+}
+
+func (rps *server) Diff(
+	ctx context.Context,
+	req *pulumirpc.DiffRequest,
+) (*pulumirpc.DiffResponse, error) {
+	panic("TODO")
+}
+
+func (rps *server) Create(
+	ctx context.Context,
+	req *pulumirpc.CreateRequest,
+) (*pulumirpc.CreateResponse, error) {
+	panic("TODO")
+}
+
+func (rps *server) Update(
+	ctx context.Context,
+	req *pulumirpc.UpdateRequest,
+) (*pulumirpc.UpdateResponse, error) {
+	panic("TODO")
+}
+
+func (rps *server) Delete(
+	ctx context.Context,
+	req *pulumirpc.DeleteRequest,
+) (*emptypb.Empty, error) {
+	panic("TODO")
 }
