365097ce5789221825e832400f7607c182165403
Cory Hall <43035978+corymhall@users.noreply.github.com>
2025-01-30 08:51:24 -0500

    Add tofu installer (#20)
    This starts the work of automating TF operations.
    
    - Use [tofudl](https://github.com/opentofu/tofudl) to dynamically
      install the opentofu binary
    - Use [terraform-exec](https://github.com/hashicorp/terraform-exec) to
      programmatically interact with the downloaded opentofu binary.
    - Create a temporary working directory where the terraform files will go
    
    This also adds a simple test to assert that the binary is downloaded and
    commands can run
    
    re #11

---
+++ pkg/tfsandbox/tofu_test.go
@@ -0,0 +1,23 @@
+package tfsandbox
+
+import (
+	"bytes"
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTofuInit(t *testing.T) {
+	tofu, err := NewTofu(context.Background())
+	assert.NoError(t, err)
+	tofu.WorkingDir()
+
+	var res bytes.Buffer
+	err = tofu.tf.InitJSON(context.Background(), &res)
+	assert.NoError(t, err)
+	t.Logf("Output: %s", res.String())
+
+	assert.NoError(t, err)
+	assert.Contains(t, res.String(), "OpenTofu initialized in an empty directory")
+}
