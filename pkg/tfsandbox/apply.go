35e065803d4d5eaddb3bb2818e2426e41c858c06
Cory Hall <43035978+corymhall@users.noreply.github.com>
2025-02-03 14:53:39 -0500

    Run terraform commands (#54)
    This adds the basic code for running tofu init, plan, apply, refresh, &
    delete.
    
    closes #23

---
+++ pkg/tfsandbox/apply.go
@@ -0,0 +1,22 @@
+package tfsandbox
+
+import (
+	"context"
+	"fmt"
+
+	tfjson "github.com/hashicorp/terraform-json"
+)
+
+// Apply runs the terraform apply command and returns the final state
+func (t *Tofu) Apply(ctx context.Context) (*tfjson.State, error) {
+	if err := t.tf.Apply(ctx); err != nil {
+		return nil, fmt.Errorf("error running tofu apply: %w", err)
+	}
+
+	state, err := t.tf.Show(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("error running tofu show: %w", err)
+	}
+
+	return state, nil
+}
