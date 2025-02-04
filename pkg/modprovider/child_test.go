7d9cc0b98301e0816e8818ea7c6cb3ed7ecedf32
Anton Tayanovskyy <anton@pulumi.com>
2025-02-04 11:54:37 -0500

    Cover Check

--- pkg/modprovider/child_test.go
+++ pkg/modprovider/child_test.go
@@ -15,15 +15,43 @@
 package modprovider
 
 import (
+	"context"
 	"testing"
+
+	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
+	pulumirpc "github.com/pulumi/pulumi/sdk/v3/proto/go"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/protobuf/types/known/structpb"
 )
+
+func TestChildResoruceTypeToken(t *testing.T) {
+	pkgName := testPackageName()
+	tok := childResourceTypeToken(pkgName, "aws_s3_bucket")
+	require.Equal(t, tokens.Type("terraform-aws-module:tf:aws_s3_bucket"), tok)
+}
 
 func TestNewChildResource(t *testing.T) {
 
 }
 
 func TestChildResourceCheck(t *testing.T) {
+	ctx := context.Background()
+	h := newChildHandler()
+	news, err := structpb.NewStruct(map[string]any{
+		childResourceAddressPropName: "module.s3_bucket.aws_s3_bucket.this[0]",
+		"force_destroy":              true,
+	})
+	require.NoError(t, err)
+	resp, err := h.Check(ctx, &pulumirpc.CheckRequest{
+		Type: "terraform-aws-module:tf:aws_s3_bucket",
+		News: news,
+	})
+	require.NoError(t, err)
 
+	checkedInputs := resp.Inputs.AsMap()
+	assert.Equal(t, string(testAddress()), checkedInputs[childResourceAddressPropName])
+	assert.Equal(t, true, checkedInputs["force_destroy"])
 }
 
 func TestChildResourceCreate(t *testing.T) {
@@ -43,3 +71,11 @@ func TestChildResourceDelete(t *testing.T) {
 }
 
 func newPlanSource() {}
+
+func testPackageName() packageName {
+	return packageName("terraform-aws-module")
+}
+
+func testAddress() ResourceAddress {
+	return ResourceAddress("module.s3_bucket.aws_s3_bucket.this[0]")
+}
