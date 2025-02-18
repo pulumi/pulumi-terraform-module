bd24a1ecf6936a437deba32d1d7b2992ebaf7486
Cory Hall <43035978+corymhall@users.noreply.github.com>
2025-02-18 13:25:17 -0500

    Handle unknown inputs to the module (#105)
    This adds support for Pulumi unknown inputs to Terraform modules. Since
    Terraform does not support unknown values I've used a proxy resource to
    represent the unknown value.
    
    I've chosen the [terraform_data](https://developer.hashicorp.com/terraform/language/resources/terraform-data) resource
    because it implements the terraform lifecycle, but doesn't actually take
    any actions. You can pass a plain input to a `terraform_data` resource
    and then reference the `output` attribute to get a computed value in
    terraform.
    
    - A single `terraform_data` resource is used to proxy all unknown inputs
    - The `terraform_data` resource is only needed in `plan` so is not
      written to state
    - The `terraform_data` resource is filtered out and not included in the
      Pulumi resource so is not shown to the user
    
    This is what the Random test plan looks like
    
    ```console
    + pulumi:pulumi:Stack: (create)
        [urn=urn:pulumi:test::ts-randmod-program::pulumi:pulumi:Stack::ts-randmod-program-test]
        + random:index/randomInteger:RandomInteger: (create)
            [urn=urn:pulumi:test::ts-randmod-program::random:index/randomInteger:RandomInteger::seed]
            [provider=urn:pulumi:test::ts-randmod-program::pulumi:providers:random::default_4_17_0::04da6b54-80e4-46f7-96ec-b56ff0331ba9]
            max : 16
            min : 1
            seed: "the-most-random-seed"
    + randmod:index:Module: (create)
        [urn=urn:pulumi:test::ts-randmod-program::randmod:index:Module::myrandmod]
        + randmod:index:ModuleState: (create)
            [urn=urn:pulumi:test::ts-randmod-program::randmod:index:Module$randmod:index:ModuleState::moduleState]
            [provider=urn:pulumi:test::ts-randmod-program::pulumi:providers:randmod::default_0_0_1::04da6b54-80e4-46f7-96ec-b56ff0331ba9]
        + randmod:tf:random_integer: (create)
            [urn=urn:pulumi:test::ts-randmod-program::randmod:index:Module$randmod:tf:random_integer::module.mymodule.random_integer.priority]
            [provider=urn:pulumi:test::ts-randmod-program::pulumi:providers:randmod::default_0_0_1::04da6b54-80e4-46f7-96ec-b56ff0331ba9]
            id       : output<string>
            max      : 10
            min      : 1
            result   : output<string>
            seed     : output<string>
        --outputs:--
        randomPriority: output<string>
    Resources:
        + 5 to create
    ```
    
    closes #28

--- pkg/tfsandbox/tf_file.go
+++ pkg/tfsandbox/tf_file.go
@@ -2,16 +2,47 @@ package tfsandbox
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 	"path"
 
-	"github.com/pulumi/pulumi-go-provider/resourcex"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
 )
 
+var (
+	terraformDataResourceType = "terraform_data"
+	terraformDataResourceName = "unknown_proxy"
+)
+
+// decode decodes a PropertyValue, recursively replacing any unknown values
+// with the unknown proxy
+func decode(pv resource.PropertyValue) (interface{}, bool) {
+	// paranoid asserts
+	// TODO: [pulumi/pulumi-terraform-module-provider#103]
+	contract.Assertf(!pv.IsSecret(), "did not expect secrets here")
+	contract.Assertf(!pv.IsAsset(), "did not expect assets here")
+	contract.Assertf(!pv.IsArchive(), "did not expect archives here")
+	contract.Assertf(!pv.IsResourceReference(), "did not expect resource references here")
+
+	// If the output value is known, process the underlying value
+	if pv.IsOutput() && pv.OutputValue().Known {
+		return pv.OutputValue().Element.MapRepl(nil, decode), true
+	}
+
+	// Replace computed's with references and stop
+	if pv.IsComputed() || (pv.IsOutput() && !pv.OutputValue().Known) {
+		return "${terraform_data.unknown_proxy.output}", true
+	}
+
+	// Otherwise continue recursive processing as before.
+	return nil, false
+}
+
 // Writes a pulumi.tf.json file in the workingDir that instructs Terraform to call a given module instance.
+// Unknown inputs (e.g. output values) are handled by using a "terraform_data" resource as a proxy
+// terraform_data resources implement the resource lifecycle, but do not perform any actions and do not
+// require you to configure a provider. see https://developer.hashicorp.com/terraform/language/resources/terraform-data
 func CreateTFFile(
 	name string, // name of the module instance
 	source TFModuleSource,
@@ -27,22 +58,34 @@ func CreateTFFile(
 		moduleProps["version"] = version
 	}
 
-	containsUnknowns := false
-	resourcex.Walk(resource.NewObjectProperty(inputs), func(pv resource.PropertyValue, ws resourcex.WalkState) {
-		if ws.Entering {
-			if pv.IsComputed() || (pv.IsOutput() && !pv.OutputValue().Known) {
-				containsUnknowns = true
-			}
-		}
-	})
-
-	// TODO: [pulumi/pulumi-terraform-module#28] Support unknown values
-	if containsUnknowns {
-		return fmt.Errorf("unknown values are not yet supported")
+	// Terraform JSON format
+	// see https://developer.hashicorp.com/terraform/language/syntax/json
+	tfFile := map[string]interface{}{
+		// NOTE: other available sections
+		// "terraform": map[string]interface{}{},
+		// "provider":  map[string]interface{}{},
+		// "locals":    map[string]interface{}{},
+		// TODO: [pulumi/pulumi-terraform-module-provider#90] propagate module outputs
+		// "output":    map[string]interface{}{},
+		// "variable":  map[string]interface{}{},
 	}
 
-	values := resourcex.Decode(inputs)
-	for k, v := range values {
+	containsUnknowns := inputs.ContainsUnknowns()
+
+	// NOTE: this should only happen at plan time. At apply time all computed values
+	// should be resolved
+	if containsUnknowns {
+		tfFile["resource"] = map[string]interface{}{
+			terraformDataResourceType: map[string]interface{}{
+				terraformDataResourceName: map[string]interface{}{
+					"input": "unknown",
+				},
+			},
+		}
+	}
+	inputsMap := inputs.MapRepl(nil, decode)
+
+	for k, v := range inputsMap {
 		// TODO: I'm only converting the top layer properties for now
 		// It doesn't look like modules have info on nested properties, typically
 		// the type looks something like `map(map(string))`.
@@ -56,16 +99,8 @@ func CreateTFFile(
 		moduleProps[tfKey] = v
 	}
 
-	tfFile := map[string]interface{}{
-		// TODO: other available sections
-		// "terraform": map[string]interface{}{},
-		// "provider":  map[string]interface{}{},
-		// "locals":    map[string]interface{}{},
-		// "output":    map[string]interface{}{},
-		// "variable":  map[string]interface{}{},
-		"module": map[string]interface{}{
-			name: moduleProps,
-		},
+	tfFile["module"] = map[string]interface{}{
+		name: moduleProps,
 	}
 
 	contents, err := json.MarshalIndent(tfFile, "", "  ")
