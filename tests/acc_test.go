c6715e7787bfec36dbb9e7a6b0e3697cbc470a38
guineveresaenger <guinevere@pulumi.com>
2025-02-24 14:59:33 -0800

    Add preview "e2e" test for a basic RDS module
    This uses the db.t4g.large but the test just crashes on the provisioning so that's fun
    
    Working preview

--- tests/acc_test.go
+++ tests/acc_test.go
@@ -374,7 +374,6 @@ func TestS3BucketModSecret(t *testing.T) {
 		"plaintext": "[{\"apply_server_side_encryption_by_default\":[{\"kms_master_key_id\":\"\",\"sse_algorithm\":\"AES256\"}]}]",
 	}).Equal(t, encyptionsConfig.Inputs["rule"])
 	integrationTest.Destroy(t)
-
 }
 
 // When writing out TF files, we need to replace data that is random with a static value
@@ -395,7 +394,7 @@ func cleanRandomDataFromTerraformArtifacts(t *testing.T, tfFilesDir string, repl
 				s = strings.ReplaceAll(s, data, replace)
 			}
 
-			err = os.WriteFile(path, []byte(s), 0600)
+			err = os.WriteFile(path, []byte(s), 0o600)
 			if err != nil {
 				return err
 			}
@@ -509,7 +508,7 @@ func TestE2eTs(t *testing.T) {
 			deleteExpect: map[string]int{
 				"delete": 9,
 			},
-			//TODO: [Fix delete-replace on null_resource](https://github.com/pulumi/pulumi-terraform-module/issues/166)
+			// TODO: [Fix delete-replace on null_resource](https://github.com/pulumi/pulumi-terraform-module/issues/166)
 			diffNoChangesExpect: map[apitype.OpType]int{
 				apitype.OpType("replace"): 1,
 				apitype.OpType("same"):    8,
@@ -691,7 +690,6 @@ func TestE2eGo(t *testing.T) {
 				tc.moduleName,
 				tc.moduleVersion,
 				tc.moduleNamespace)
-
 			previewResult := e2eTest.Preview(t)
 			autogold.Expect(tc.previewExpect).Equal(t, previewResult.ChangeSummary)
 
@@ -704,6 +702,62 @@ func TestE2eGo(t *testing.T) {
 			destroyResult := e2eTest.Destroy(t)
 			autogold.Expect(&tc.deleteExpect).Equal(t, destroyResult.Summary.ResourceChanges)
 
+		})
+	}
+}
+
+// Some resources take minutes to provision and destroy.
+// TestPreview is for such resources, where we only verify that preview works as expected.
+func TestPreview(t *testing.T) {
+	type testCase struct {
+		name            string // Must be same as project folder in testdata/programs/ts
+		moduleName      string
+		moduleVersion   string
+		moduleNamespace string
+		previewExpect   map[apitype.OpType]int
+	}
+
+	testcases := []testCase{
+		{
+			name:            "rdsmod",
+			moduleName:      "terraform-aws-modules/rds/aws",
+			moduleVersion:   "6.10.0",
+			moduleNamespace: "rds",
+			previewExpect: map[apitype.OpType]int{
+				apitype.OpType("create"): 6,
+			},
+		},
+	}
+
+	for _, tc := range testcases {
+		tc := tc
+		localProviderBinPath := ensureCompiledProvider(t)
+		skipLocalRunsWithoutCreds(t)
+		t.Run(tc.name, func(t *testing.T) {
+			testProgram := filepath.Join("testdata", "programs", "ts", tc.name)
+			localPath := opttest.LocalProviderPath("terraform-module", filepath.Dir(localProviderBinPath))
+			previewTest := pulumitest.NewPulumiTest(t, testProgram, localPath)
+
+			// Get a prefix for resource names
+			prefix := generateTestResourcePrefix()
+
+			// Set prefix via config
+			previewTest.SetConfig(t, "prefix", prefix)
+
+			// Generate package
+			pulumiPackageAdd(
+				t,
+				previewTest,
+				localProviderBinPath,
+				tc.moduleName,
+				tc.moduleVersion,
+				tc.moduleNamespace)
+
+			// Preview
+			previewResult := previewTest.Preview(t)
+			t.Log(previewResult.StdOut)
+			t.Log(previewResult.StdErr)
+			autogold.Expect(tc.previewExpect).Equal(t, previewResult.ChangeSummary)
 		})
 	}
 }
@@ -729,7 +783,7 @@ func TestDiffDetail(t *testing.T) {
 	// Up
 	diffDetailTest.Up(t)
 
-	//Change program to remove the module input `server_side_encryption_configuration`
+	// Change program to remove the module input `server_side_encryption_configuration`
 	diffDetailTest.UpdateSource(t, filepath.Join("testdata", "programs", "ts", "s3bucketmod", "updates"))
 
 	// Preview
@@ -1005,7 +1059,6 @@ func TestDeleteLambda(t *testing.T) {
 	} else {
 		t.Fatalf("encountered unexpected error verifying log group was deleted: %v ", err)
 	}
-
 }
 
 // runPreviewWithPlanDiff runs a pulumi preview that creates a plan file
---
+++ tests/testdata/programs/ts/rdsmod/index.ts
@@ -0,0 +1,21 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as rds from "@pulumi/rds";
+
+
+const testrds = new rds.Module("test-rds", {
+    identifier: "test-rds-module",
+    engine: "mysql",
+    instance_class: "db.t3.micro",
+    allocated_storage: 20,
+    db_name: "testrdsmoduledatabase",
+    username: "pulumipus",
+    password: "hawaii",
+    skip_final_snapshot: true,
+    deletion_protection: false,
+
+    // DB parameter group
+    family: "mysql8.0",
+
+    // DB option group
+    major_engine_version: "8.0",
+})
