95f730fa9ee722bbd92eac07336519f36ff17ee2
Cory Hall <43035978+corymhall@users.noreply.github.com>
2025-02-27 13:31:32 -0500

    Add tests for different schema types (#154)
    This adds some tests to test the create plan for different Terraform
    schema types. This enables us to compare how Terraform represents different
    types of data with how we end up representing the data as a
    resource.PropertyValue.
    
    re #114

--- tests/acc_test.go
+++ tests/acc_test.go
@@ -587,26 +587,6 @@ func pulumiConvert(t *testing.T, localProviderBinPath, sourceDir, targetDir, lan
 	}
 }
 
-// Skip the test if it is being run locally without cloud credentials being configured.
-func skipLocalRunsWithoutCreds(t *testing.T) {
-	if _, ci := os.LookupEnv("CI"); ci {
-		return // never skip when in CI
-	}
-
-	awsConfigured := false
-	for _, envVar := range os.Environ() {
-		if strings.HasPrefix(strings.ToUpper(envVar), "AWS_ACCESS_KEY_ID") {
-			awsConfigured = true
-		}
-		if strings.HasPrefix(strings.ToUpper(envVar), "AWS_PROFILE") {
-			awsConfigured = true
-		}
-	}
-	if !awsConfigured {
-		t.Skip("AWS configuration such as AWS_PROFILE env var is required to run this test")
-	}
-}
-
 func pulumiPackageAdd(
 	t *testing.T,
 	pt *pulumitest.PulumiTest,
