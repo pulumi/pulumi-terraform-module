fb0c12656016f9bafc3b3c69e1ca34a2c6dc5ebc
guineveresaenger <guinevere@pulumi.com>
2025-03-18 16:29:25 -0700

    Getting to a plugin install error

--- tests/testdata/programs/dotnet/s3bucketmod/Program.cs
+++ tests/testdata/programs/dotnet/s3bucketmod/Program.cs
@@ -1,20 +1,45 @@
 ﻿using Pulumi;
 using Pulumi.Bucket;
+using System.Threading.Tasks;
 
 class Program
 {
-    static void Main()
+    static Task Main()
     {
-        var config = new Config();
-        var prefix = config.Get("prefix") ?? Deployment.Instance.StackName;
-
-        var testBucket = new Pulumi.Bucket("test-bucket", new Pulumi.Bucket.BucketArgs
+        return Deployment.RunAsync(() =>
         {
-            BucketName = $"{prefix}-test-bucket",
-        });
+            // Create your stack here inside RunAsync
 
-//        // Export the bucket ARN
-//        var bucketArn = testBucket.Arn.Apply(arn => arn);
-//        Deployment.Instance.Export("bucketARN", bucketArn);
+            var myBucketModule = new Module("myBucketModule", new ModuleArgs
+            {
+                Bucket = "guins-test-bucket",  // Example argument
+                // Add other arguments here
+            });
+
+            return Task.CompletedTask;
+        });
     }
 }
+
+
+
+
+
+
+//class Program
+//{
+//    static void Main()
+//    {
+////        var config = new Config();
+////        var prefix = config.Get("prefix") ?? Deployment.Instance.StackName;
+//
+//        var testBucket = new Pulumi.Bucket.Module("test-bucket", new Pulumi.Bucket.ModuleArgs
+//        {
+//            Bucket = "guin-test-bucket",
+//        });
+//
+////        // Export the bucket ARN
+////        var bucketArn = testBucket.Arn.Apply(arn => arn);
+////        Deployment.Instance.Export("bucketARN", bucketArn);
+//    }
+//}
