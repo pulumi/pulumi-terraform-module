1a5ac4fbfe4473ef43af43c1b7a02e4d20f059cb
Guinevere Saenger <guinevere@pulumi.com>
2025-03-20 10:42:01 -0700

    Add basic e2e test for Dotnet (#230)
    * First stab at e2e dotnet test
    
    * Getting to a plugin install error
    
    * More tweaks
    
    * This works locally with pulumi@master. Awaiting Pulumi release.
    
    Don't need to inlude the Bucket Item Group
    
    cleanup

---
+++ tests/testdata/programs/dotnet/s3bucketmod/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Pulumi;
+using Pulumi.Bucket;
+
+return await Deployment.RunAsync(() =>
+{
+    // Add your resources here
+    // e.g. var resource = new Resource("name", new ResourceArgs { });
+    var config = new Config();
+    var prefix = config.Get("prefix") ?? Deployment.Instance.StackName;
+    var myBucketModule = new Pulumi.Bucket.Module("myBucketModule", new ModuleArgs
+            {
+                Bucket = $"{prefix}-test-bucket",  // Example argument
+                // Add other arguments here
+            });
+});
