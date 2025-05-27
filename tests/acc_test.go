ced684b2f4bcb6e4027c4326e75ba93e45648400
Anton Tayanovskyy <anton@pulumi.com>
2025-05-27 12:51:48 -0400

    Fix TestS3BucketModSecret

--- tests/acc_test.go
+++ tests/acc_test.go
@@ -499,31 +499,11 @@ func TestS3BucketModSecret(t *testing.T) {
 		"bucket:tf:aws_s3_bucket_server_side_encryption_configuration",
 	)
 
-	if viewsEnabled {
-		autogold.Expect(map[string]interface{}{
-			"bucket": "576852-test-bucket", "expected_bucket_owner": "",
-			"id": "576852-test-bucket",
-			"rule": map[string]interface{}{
-				"4dabf18193072939515e22adb298388d": "1b47061264138c4ac30d75fd1eb44270",
-				//nolint:lll
-				"plaintext": `[{"apply_server_side_encryption_by_default":[{"kms_master_key_id":"","sse_algorithm":"AES256"}]}]`,
-			},
-		}).Equal(t, encrConf.Inputs)
-
-	} else {
-		autogold.Expect(map[string]interface{}{
-			"__address":             "module.test-bucket.aws_s3_bucket_server_side_encryption_configuration.this[0]",
-			"__module":              "urn:pulumi:test::ts-s3bucketmod-program::bucket:index:Module::test-bucket",
-			"bucket":                "174703-test-bucket",
-			"expected_bucket_owner": "",
-			"id":                    "174703-test-bucket",
-			"rule": map[string]interface{}{
-				"4dabf18193072939515e22adb298388d": "1b47061264138c4ac30d75fd1eb44270",
-				//nolint:lll
-				"plaintext": `[{"apply_server_side_encryption_by_default":[{"kms_master_key_id":"","sse_algorithm":"AES256"}]}]`,
-			},
-		}).Equal(t, encrConf.Inputs)
-	}
+	// Testing only the `rule` value to avoid capturing random-generated ID from the state.
+	autogold.Expect(map[string]interface{}{
+		"4dabf18193072939515e22adb298388d": "1b47061264138c4ac30d75fd1eb44270",
+		"plaintext":                        `[{"apply_server_side_encryption_by_default":[{"kms_master_key_id":"","sse_algorithm":"AES256"}]}]`,
+	}).Equal(t, encrConf.Inputs["rule"])
 
 	autogold.Expect(map[string]interface{}{}).Equal(t, encrConf.Outputs)
 }
