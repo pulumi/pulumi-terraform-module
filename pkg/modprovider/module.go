64ed18d7706a4b79fa9396b96203c68fbd34408f
Anton Tayanovskyy <anton@pulumi.com>
2025-05-30 14:56:35 -0400

    Lint/typo

--- pkg/modprovider/module.go
+++ pkg/modprovider/module.go
@@ -437,7 +437,7 @@ func (h *moduleHandler) Delete(
 
 	// TODO[pulumi/pulumi-terraform-module#247] once the engine is ready to receive view steps multiple times, the
 	// code here should be able to plan the destroy and send the view-steps right after planning, and then send
-	// udpated view-steps after the actual destroy operation finishes. This should improve user latency to first
+	// updated view-steps after the actual destroy operation finishes. This should improve user latency to first
 	// seeing the changes.
 	stateBeforeDestroy, err := tf.Show(ctx, logger)
 	if err != nil {
